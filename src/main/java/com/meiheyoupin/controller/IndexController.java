3fd6a5911f905e2d622ae60e1a698d0bc2b45762
luxiaotao1123 <1341870251@qq.com>
2018-01-12 12:34:21 +0800

    das

--- src/main/java/com/meiheyoupin/controller/IndexController.java
+++ src/main/java/com/meiheyoupin/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/admin")
+    @RequestMapping("/")
     public String admin(){
         return "login";
     }
