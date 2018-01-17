1edc80ca428e4d58f31c4fa2cc84b05d5bfa2f9f
weitao <1525642679@qq.com>
2018-01-17 19:30:21 +0800

    dsa

--- src/main/java/com/meiheyoupin/common/SMSUtils.java
+++ src/main/java/com/meiheyoupin/common/SMSUtils.java
@@ -201,7 +201,7 @@ public class SMSUtils {
         request.setSignName("美盒优品");
 
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_122280484");
+        request.setTemplateCode("SMS_122295572");
 
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
