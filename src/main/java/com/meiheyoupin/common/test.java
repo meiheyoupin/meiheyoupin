7d5858ddee3e3f8a2afe354c761149acb8a9fc33
luxiaotao1123 <1341870251@qq.com>
2018-02-02 19:35:50 +0800

    陆晓涛

--- src/main/java/com/meiheyoupin/common/test.java
+++ src/main/java/com/meiheyoupin/common/test.java
@@ -1,12 +1,50 @@
 package com.meiheyoupin.common;
 
-import com.meiheyoupin.utils.XMailUtils;
+import com.aliyuncs.exceptions.ClientException;
 
-/**
- * Created by w2tto on 2018/2/2.
- */
+import java.util.concurrent.locks.ReentrantLock;
+
+
 public class test {
-    public static void main(String[] args) {
-        XMailUtils.sendMail("1341870251@qq.com","SUCCESS");
+
+    private static int j = 3;
+
+    public static ReentrantLock lock = new ReentrantLock();
+
+    private static String o = "str";
+
+    public static void main(String[] args) throws ClientException, InterruptedException {
+        test myTest = new test();
+        System.out.println("start");
+        myTest.myRun();
+    }
+
+    private void myRun(){
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    int frequency = 0;
+                    for (int i = 0;i<3;i++){
+                        Thread.sleep(2000);
+                        if (1==j){
+                            break;
+                        }
+                        frequency += 1;
+                        System.out.println(frequency);
+                        if (frequency<3){
+                            System.out.println("============Hello World====================");
+                        }else {
+                            System.out.println("===============BOOM========================");
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+
     }
 }
